9fba7491bd29e48f1b5e44b9846270791846f9f2
eSynapsX <info@synapsx.com>
2024-07-18 12:28:27 +0200

    fix: wait until dom is loaded

--- src/index.js
+++ src/index.js
@@ -1,24 +1,25 @@
 import './index.css';
 
 
-var video = document.getElementById("video-intro");
-var intro = document.getElementById("intro");
-var subtitle = document.getElementById("subtitle");
-var logoNav = document.getElementById("logo-nav");
+function init() {
+    var video = document.getElementById("video-intro");
+    var intro = document.getElementById("intro");
+    var subtitle = document.getElementById("subtitle");
+    var logoNav = document.getElementById("logo-nav");
+
+    video.addEventListener('loadeddata', (e) => {
+        //Video should now be loaded but we can add a second check
+        console.log('Video is loaded: ', video.readyState);
+        if (video.readyState >= 3) {
+            intro.classList.add('intro-animation')
+            subtitle.classList.add('subtitle-animation')
+            logoNav.classList.add('logo-nav-animation')
+        }
+    });
+}
 
 window.imageClick = (url) => {
     window.location = url;
 }
 
-console.log(video)
-
-video.addEventListener('loadeddata', (e) => {
-    //Video should now be loaded but we can add a second check
-    console.log('Video is loaded: ', video.readyState);
-    if (video.readyState >= 3) {
-        intro.classList.add('intro-animation')
-        subtitle.classList.add('subtitle-animation')
-        logoNav.classList.add('logo-nav-animation')
-    }
-});
-
+document.addEventListener("DOMContentLoaded", init, false);
